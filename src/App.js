11455453e8deb776f15cf99d68d174e3433d0ae8
Raksha25 <guptaraksha712@gmail.com>
2020-09-04 19:22:08 +0530

    CU-179ecn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Topbar from "./components/Page1/Topbar";
-import About from "./components/Page1/About";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+import LandingPage from "./Pages/LandingPage";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import TermsOfUse from "./Pages/TermsOfUse";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
-    <div>
-      <Topbar></Topbar>
-      <About></About>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/privacy-policy" component={PrivacyPolicy} />
+        <Route exact path="/terms-of-use" component={TermsOfUse} />
+        <Route exact path="/sign-up" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
